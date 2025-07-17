35dc3a60be21fe4b57b0ae62687b40b761150fc5
tpgj9 <tpgj98@naver.com>
2025-07-18 04:33:17 +0900

    [FEAT] Add repository layer

---
+++ src/main/kotlin/kr/hhplus/be/server/repository/mock/MockUserRepository.kt
@@ -0,0 +1,49 @@
+package kr.hhplus.be.server.repository.mock
+
+import kr.hhplus.be.server.domain.users.User
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Repository
+import java.util.concurrent.ConcurrentHashMap
+
+@Repository
+class MockUserRepository {
+    private val log = LoggerFactory.getLogger(MockUserRepository::class.java)
+    private val users = ConcurrentHashMap<String, User>()
+
+    fun save(user: User): User {
+        val isNewUser = !users.containsKey(user.userId)
+
+        users[user.userId] = user
+
+        if (isNewUser) {
+            log.info("Inserted new User: ${user.userId}, Point: ${user.availablePoint}")
+        } else {
+            log.info("Updated User: ${user.userId}, Point: ${user.availablePoint}")
+        }
+
+        return user
+    }
+
+    fun findByUserId(id: String): User? {
+        return users[id]
+    }
+
+    fun updatePoint(userId: String, point: Int): User {
+        val user = users[userId]
+            ?: throw IllegalArgumentException("User not found: $userId")
+
+        when {
+            point > 0 -> {
+                user.chargePoint(point)
+            }
+            point < 0 -> {
+                user.usePoint(-point)
+            }
+            else -> {
+            }
+        }
+
+        users[userId] = user
+        return user
+    }
+}
