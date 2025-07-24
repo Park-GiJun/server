33c73b28106caa9808eead81744415783bb22c69
tpgj9 <tpgj98@naver.com>
2025-07-24 22:43:14 +0900

    feat: persistent layer

---
+++ src/main/kotlin/kr/hhplus/be/server/infrastructure/adapter/out/persistence/concert/ConcertRepositoryImpl.kt
@@ -0,0 +1,25 @@
+package kr.hhplus.be.server.infrastructure.adapter.out.persistence.concert
+
+import kr.hhplus.be.server.application.port.out.concert.ConcertRepository
+import kr.hhplus.be.server.domain.concert.Concert
+import kr.hhplus.be.server.infrastructure.adapter.out.persistence.concert.mock.MockConcertRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+class ConcertRepositoryImpl(
+    private val concertRepository: MockConcertRepository
+) : ConcertRepository {
+    override fun save(concert: Concert): Concert {
+        return concertRepository.save(concert)
+    }
+
+    override fun findConcertList(): List<Concert> {
+        return concertRepository.findConcertList()
+    }
+
+    override fun findByConcertId(concertId: Long): Concert? {
+        return concertRepository.findByConcertId(concertId)
+    }
+
+
+}
