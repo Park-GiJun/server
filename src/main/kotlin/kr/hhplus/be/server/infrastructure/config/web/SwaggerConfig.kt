c8f7d5e4f27c27bf08a3b0e6d9e12c66de5a7258
tpgj9 <tpgj98@naver.com>
2025-07-24 22:32:16 +0900

    chore: configuration

--- src/main/kotlin/kr/hhplus/be/server/infrastructure/config/web/SwaggerConfig.kt
+++ src/main/kotlin/kr/hhplus/be/server/infrastructure/config/web/SwaggerConfig.kt
@@ -1,4 +1,4 @@
-package kr.hhplus.be.server.config
+package kr.hhplus.be.server.infrastructure.config.web
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import io.swagger.v3.oas.annotations.info.Info
