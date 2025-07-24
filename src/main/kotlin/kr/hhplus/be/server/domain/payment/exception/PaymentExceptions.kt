c7da785b570c5d82ec82dead8ae7e78534170c63
tpgj9 <tpgj98@naver.com>
2025-07-24 22:42:12 +0900

    feat: domain layer

---
+++ src/main/kotlin/kr/hhplus/be/server/domain/payment/exception/PaymentExceptions.kt
@@ -0,0 +1,12 @@
+package kr.hhplus.be.server.domain.payment.exception
+
+import kr.hhplus.be.server.domain.common.exception.BusinessRuleViolationException
+import kr.hhplus.be.server.domain.common.exception.EntityNotFoundException
+
+class PaymentNotFoundException(paymentId: Long) : EntityNotFoundException("Payment", paymentId.toString())
+
+class PaymentAlreadyProcessedException(reservationId: Long) :
+    BusinessRuleViolationException("Payment already processed for reservation $reservationId")
+
+class InvalidPaymentAmountException(amount: Int) :
+    BusinessRuleViolationException("Invalid payment amount: $amount. Amount must be positive")
