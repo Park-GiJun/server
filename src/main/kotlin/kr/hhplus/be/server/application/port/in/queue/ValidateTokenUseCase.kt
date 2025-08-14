6b076a6ee83df6ffb2636a7e8021d4a85c21b088
tpgj9 <tpgj98@naver.com>
2025-08-15 01:24:08 +0900

    refactor: remove unused class

--- src/main/kotlin/kr/hhplus/be/server/application/port/in/queue/ValidateTokenUseCase.kt
+++
@@ -1,9 +0,0 @@
-package kr.hhplus.be.server.application.port.`in`.queue
-
-import kr.hhplus.be.server.application.dto.queue.ValidateTokenCommand
-import kr.hhplus.be.server.application.dto.queue.ValidateTokenResult
-
-interface ValidateTokenUseCase {
-    fun validateActiveToken(command: ValidateTokenCommand): ValidateTokenResult
-    fun validateActiveTokenForConcert(command: ValidateTokenCommand): ValidateTokenResult
-}
